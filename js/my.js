17e971340a6490465615feef75ee7ccbb6fbc8b9
Yilin <revetice@gmail.com>
2016-04-26 21:41:14 +0800

    update

---
+++ js/my.js
@@ -0,0 +1,12 @@
+$(document).ready(function($){
+	"use_strict";
+
+    $('a.page-scroll').on('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+});
